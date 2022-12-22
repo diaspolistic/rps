fd1e8120aa567b699ae1c8bafc339d196b9b6816
diaspolistic <charlene.connorshg@gmail.com>
2022-12-22 08:47:11 +0100

    Declare each element (Rock, Paper and scissor) and assign a random round number

---
+++ rps.js
@@ -0,0 +1,20 @@
+// Declare each element
+// Assign a random number to each element ('Rock', 'Paper', 'Scissor')
+// Round the number for a cleaner code
+
+const Rock = Math.floor(Math.random() + 1);
+const Paper = Math.floor(Math.random() + 2);
+const Scissor = Math.floor(Math.random() + 3);
+
+function getComputerChoice(Rock, Paper, Scissor) {
+return Rock; 
+}
+// If the number is x, print 'Rock'
+// If the number is y, print 'Paper'
+// Otherwise print 'Scissor'
+// When I input console.log(getComputerChoice), 
+// Print randomly 'Rock', 'Paper' or 'Scissor'
+
+
+
+    
