8babd8de2357aac53fec22b55ce5e07b5310191c
diaspolistic <charlene.connorshg@gmail.com>
2022-12-25 18:19:24 +0100

    Write a function that let the user input his/her choice between "rock", "paper" or "scissor". If the user type one of the three words, the choice is displayed, if not, a prompt appear asking the user to type one of the three choice. COMMENT:  Long code, will need to optimize it using logical "OR".

--- rps.js
+++ rps.js
@@ -1,12 +1,14 @@
 // // Declare a function that let the computer choose a random number between 1, 2 and 3 (OK)
-// function randomNumber(min, max) {
-//     min = Math.ceil(1);
-//     max = Math.floor(3);
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-//     } 
+
 
 // // Declare a function that assigns a number to each element "Rock", "Paper" and "Scissor"
 // function getComputerChoice(Rock, Paper, Scissors) {
+//     function randomNumber(min, max) {
+//         min = Math.ceil(1);
+//         max = Math.floor(3);
+//         return Math.floor(Math.random() * (max - min + 1)) + min;
+//         } 
+
 //     if (randomNumber() === 1) {
 //         return('Rock');
 //     } else if (randomNumber() === 2) {
@@ -17,20 +19,31 @@
 // }
 
 // Write a function that plays a single round of Rock paper Scissors
-// When the computer plays first
-function computerTurn() {
-    alert('It\'s the computer turn');
-    // And the user click 'OK'
+function rpsGame(computerSelection, playerSelection) {
+    // The user plays the first round
+    playerSelection = prompt('It\'s your turn. Type either rock, paper or scissor');
+    if (playerSelection === 'rock') {
+        return(playerSelection);
 
-    // Declare a function that let the computer choose a random number between 1, 2 and 3 (OK)
-    function randomNumber(min, max) {
-        min = Math.ceil(1);
-        max = Math.floor(3);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-        } 
+    } else if (playerSelection === 'paper') {
+        return(playerSelection);
+
+    } else if (playerSelection === 'scissors')
+        return(playerSelection);
+
+    else { 
+        prompt('You can only type either rock, paper or scissor. Try again');
+    }
     
-    // Declare a function that assigns a number to each element "Rock", "Paper" and "Scissor"
+    
+    }
+        
+
+
+    {
+        // computerSelection = prompt('It\'s the computer turn')
     function getComputerChoice(Rock, Paper, Scissors) {
+        // Declare a function that assigns a number to each element "Rock", "Paper" and "Scissor"
         if (randomNumber() === 1) {
             alert('Rock');
         } else if (randomNumber() === 2) {
@@ -39,6 +52,13 @@ function computerTurn() {
             alert('Scissors');
         }
     }
+    // Declare a function that let the computer choose a random number between 1, 2 and 3 (OK)
+        function randomNumber(min, max) {
+            min = Math.ceil(1);
+            max = Math.floor(3);
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+            } 
+    
 }
 // Then a pop up appears and asks the playerSelection to play
 // The player can use any case variation (case insensitive) 
