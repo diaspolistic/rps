e4789a4c3759e21c9686e24c6db569e95a4589ae
diaspolistic <charlene.connorshg@gmail.com>
2022-12-26 20:57:10 +0100

    Change return to alert to show to the user the element that was played + make it possible to execute the code after. + Add alert to indicate to the user that it is the computer turn. + assign computerSelection parameter to the getComputerChoice function. + Add string for user choice and computer choice to give a sentence saying what was the choice.

--- rps.js
+++ rps.js
@@ -20,43 +20,46 @@
 
 // Write a function that plays a single round of Rock paper Scissors
 function rpsGame(computerSelection, playerSelection) {
-    The user plays the first round
+    // // The user plays the first round
     playerSelection = prompt('It\'s your turn. Type either rock, paper or scissors');
+
     if (playerSelection === 'rock') {
-        return(playerSelection);
+        alert('You\'ve played' + ' ' + playerSelection);
 
     } else if (playerSelection === 'paper') {
-        return(playerSelection);
+        alert('You\'ve played' + ' ' + playerSelection);
 
-    } else if (playerSelection === 'scissors')
-        return(playerSelection);
+    } else if (playerSelection === 'scissors') {
+        alert('You\'ve played' + ' ' + playerSelection);
 
+    } 
     else { 
         prompt('You can only type either rock, paper or scissors. Try again');
     }
-    
-    computerSelection = confirm('It\'s the computer turn');
-    {
-        // computerSelection = prompt('It\'s the computer turn')
-    function getComputerChoice(Rock, Paper, Scissors) {
+// When the user input is returned, a pop up appears saying that it is the computer turn 
+    alert('It\'s the computer turn');
+    computerSelection = getComputerChoice();
+
+    function getComputerChoice() {
         // Declare a function that assigns a number to each element "Rock", "Paper" and "Scissor"
         if (randomNumber() === 1) {
-            alert('Rock');
+            confirm('The computer played' + ' ' + 'Rock');
         } else if (randomNumber() === 2) {
-            alert('Paper');
+            confirm('The computer played' + ' ' + 'Paper');
         } else {
-            alert('Scissors');
+            confirm('The computer played' + ' ' + 'Scissors');
         }
     }
+       
     // Declare a function that let the computer choose a random number between 1, 2 and 3 (OK)
-        function randomNumber(min, max) {
-            min = Math.ceil(1);
-            max = Math.floor(3);
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-            } 
-    
+    function randomNumber(min, max) {
+        min = Math.ceil(1);
+        max = Math.floor(3);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+        } 
 }
-}
+
+
 // Then a pop up appears and asks the playerSelection to play
 // The player can use any case variation (case insensitive) 
 // After the computer and the playerSelection played
