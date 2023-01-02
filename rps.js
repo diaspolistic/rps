9dfe55c4ff829fb9997fbddb203186261d5f9b19
diaspolistic <charlene.connorshg@gmail.com>
2023-01-02 10:50:49 +0100

    Move functions getPlayerChoice() and getComputerChoice() out of the game() function scope for hoisting to work properly

--- rps.js
+++ rps.js
@@ -11,6 +11,51 @@ let loose = 'You loose !';
 let playerScore = 0;
 let computerScore = 0;
 
+// The user plays the first round
+ function getPlayerChoice() {
+    playerSelection = prompt('It\'s your turn. Type either rock, paper or scissors');
+
+    if (playerSelection.toLowerCase() === rock) {
+        alert('You\'ve played' + ' ' + playerSelection.toLowerCase());
+
+    } else if (playerSelection.toLowerCase() === paper) {
+        alert('You\'ve played' + ' ' + playerSelection.toLowerCase());
+
+    } else if (playerSelection.toLowerCase() === scissors) {
+        alert('You\'ve played' + ' ' + playerSelection.toLowerCase());
+
+    } 
+    else { 
+        alert('You can only type either rock, paper or scissors. Try again');
+        console.log(getPlayerChoice());
+    }
+}
+
+// The computer plays the second round
+function getComputerChoice() {
+    randomNumber();
+
+// Declare a function that let the computer choose a random number between 1, 2 and 3
+    function randomNumber(min, max) {
+        min = Math.ceil(1);
+        max = Math.floor(3);
+        return(Math.floor(Math.random() * (max - min + 1)) + min);
+        } 
+    
+// Assign a value (Rock, Paper or Scissors) to computerSelection parameter according to the randomNumber    
+    if (randomNumber() === 1) {
+        computerSelection = rock;
+        alert('The computer played' + ' ' + rock);
+    } else if (randomNumber() === 2) {
+        computerSelection = paper;
+        alert('The computer played' + ' ' + paper);
+    } else if(randomNumber() === 3) {
+        computerSelection = scissors;
+        alert('The computer played' + ' ' + scissors);
+    }
+}
+
+
 // Write a function that create a loop to play 5 rounds and the final winner
 game();
 function game() { 
@@ -25,58 +70,21 @@ function game() {
         alert('Oh no, this is so sad, you\'ve lost the Rock, paper, Scissors game');
 
     } else if (playerScore === computerScore) {
-        alert('Well, it looks like the computer and you have the same level... Maybe start another game to find out who is the true Rock, Paper, Scissors champion !')
+        alert('Well, it looks like the computer and you have the same level... Maybe start another game to find out who is the true Rock, Paper, Scissors champion !');
     }
     console.log('Here is the final score : '+ 'You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.');
 
 // Write a function that plays a single round of Rock paper Scissors
 function playRound(computerSelection, playerSelection) {
-    
-    // // The user plays the first round
     getPlayerChoice();
-    function getPlayerChoice() {
-        playerSelection = prompt('It\'s your turn. Type either rock, paper or scissors');
-
-        if (playerSelection.toLowerCase() === rock) {
-            alert('You\'ve played' + ' ' + playerSelection.toLowerCase());
-
-        } else if (playerSelection.toLowerCase() === paper) {
-            alert('You\'ve played' + ' ' + playerSelection.toLowerCase());
-
-        } else if (playerSelection.toLowerCase() === scissors) {
-            alert('You\'ve played' + ' ' + playerSelection.toLowerCase());
-
-        } 
-        else { 
-            alert('You can only type either rock, paper or scissors. Try again');
-            getPlayerChoice();
-        }
-    }
-
     getComputerChoice();
-    function getComputerChoice() {
-    // Declare a function that let the computer choose a random number between 1, 2 and 3
-        function randomNumber(min, max) {
-            min = Math.ceil(1);
-            max = Math.floor(3);
-            return(Math.floor(Math.random() * (max - min + 1)) + min);
-            } 
-        
-    // Assign a value (Rock, Paper or Scissors) to computerSelection parameter according to the randomNumber    
-        if (randomNumber() === 1) {
-            computerSelection = rock;
-            alert('The computer played' + ' ' + rock);
-        } else if (randomNumber() === 2) {
-            computerSelection = paper;
-            alert('The computer played' + ' ' + paper);
-        } else if(randomNumber() === 3) {
-            computerSelection = scissors;
-            alert('The computer played' + ' ' + scissors);
-        }
-    }
+    result();
+
+   
+
+   
 
 // Compare the computerChoice and the playerSelection and alert the winner/looser 
-    result();
     function result() {
         if (computerSelection === rock && playerSelection === paper) {
             playerSelection = win;
@@ -100,6 +108,8 @@ function playRound(computerSelection, playerSelection) {
             alert('It\'s a tie.');
         } 
 
+        roundScore();
+        function roundScore() {
         if (playerSelection === win) {
             playerScore = playerScore + 1;
             console.log('You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.' + ' You\'re ahead !');
@@ -111,10 +121,9 @@ function playRound(computerSelection, playerSelection) {
         } else if (playerScore === computerScore) { 
             console.log('You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.' + ' It looks like a tie so far.')
         }
+
+    }
      }
 
     }
 }
-// // console.log(playRound());
-// console.log(game());
-
