7d5a1c7a21641e2bafd0b43f80dcf9975270115d
diaspolistic <charlene.connorshg@gmail.com>
2023-01-02 14:35:23 +0100

    Reorder functions and seperate them to make them more readable, a better scope and function hoisting

--- rps.js
+++ rps.js
@@ -11,8 +11,14 @@ let loose = 'You loose !';
 let playerScore = 0;
 let computerScore = 0;
 
-// The user plays the first round
- function getPlayerChoice(playerSelection) {
+
+function playRound(computerSelection, playerSelection) {
+    getPlayerChoice();
+    getComputerChoice();
+    roundResult();
+
+// The user plays first
+ function getPlayerChoice() {
     playerSelection = prompt('It\'s your turn. Type either rock, paper or scissors');
 
     if (playerSelection.toLowerCase() === rock) {
@@ -30,28 +36,70 @@ let computerScore = 0;
         console.log(getPlayerChoice());
     }
 }
+// The computer plays seond
+function getComputerChoice() {
 
-// The computer plays the second round
-function getComputerChoice(computerSelection) {
+    // Declare a function that let the computer choose a random number between 1, 2 and 3
+        function randomNumber(min, max) {
+            min = Math.ceil(1);
+            max = Math.floor(3);
+            return(Math.floor(Math.random() * (max - min + 1)) + min);
+            } 
+        computerSelection = randomNumber();
 
-// Declare a function that let the computer choose a random number between 1, 2 and 3
-    function randomNumber(min, max) {
-        min = Math.ceil(1);
-        max = Math.floor(3);
-        return(Math.floor(Math.random() * (max - min + 1)) + min);
-        } 
-    
-// Assign a value (Rock, Paper or Scissors) to computerSelection parameter according to the randomNumber    
-    if (randomNumber() === 1) {
-        computerSelection = rock;
-        alert('The computer played' + ' ' + rock);
-    } else if (randomNumber() === 2) {
-        computerSelection = paper;
-        alert('The computer played' + ' ' + paper);
-    } else if(randomNumber() === 3) {
-        computerSelection = scissors;
-        alert('The computer played' + ' ' + scissors);
+    // Assign a value (rock, paper or scissors) to computerSelection parameter according to the randomNumber    
+        if (computerSelection === 1) {
+            computerSelection = rock;
+            alert('The computer played' + ' ' + rock);
+        } else if (computerSelection === 2) {
+            computerSelection = paper;
+            alert('The computer played' + ' ' + paper);
+        } else if(computerSelection === 3) {
+            computerSelection = scissors;
+            alert('The computer played' + ' ' + scissors);
+        }
     }
+
+// Give the result of each round 
+    function roundResult() {
+        if (computerSelection === rock && playerSelection === paper) {
+            playerSelection = win;
+            playerScore = playerScore + 1;
+            alert(win + ' Paper beats Rock ');
+            console.log('You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.');
+        } else if (computerSelection === rock && playerSelection === scissors) {
+            playerSelection = loose;
+            computerScore = computerScore + 1;
+            alert(loose + ' Rock beats Scissors');
+            console.log('You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.');
+        } else if (computerSelection === paper && playerSelection === rock) {
+            playerSelection = loose;
+            computerScore = computerScore + 1;
+            alert(loose + ' Paper beats Rock');
+            console.log('You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.');
+        } else if (computerSelection === paper && playerSelection === scissors) {
+            playerSelection = win;
+            playerScore = playerScore + 1;
+            alert(win + ' Scissors beats Paper');
+            console.log('You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.');
+        } else if (computerSelection === scissors && playerSelection === paper) {
+            playerSelection = loose;
+            computerScore = computerScore + 1;
+            alert(loose + ' ' + 'Scissors beats Paper');
+            console.log('You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.');
+        } else if (computerSelection === scissors && playerSelection === rock) {
+            playerSelection = win;
+            playerScore = playerScore + 1;
+            alert(win + ' Rock beats Scissors');
+            console.log('You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.');
+        } else if (computerSelection === playerSelection) {
+            alert('It\'s a tie.');
+            console.log('You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.');
+        } 
+
+}
+
+
 }
 
 // Write a function that create a loop to play 5 rounds and the final winner
@@ -60,68 +108,50 @@ function game() {
     for (let i = 0; i < 5; i++) {
         playRound();
     }
-    function playRound(computerSelection, playerSelection) {
-        getPlayerChoice(playerSelection);
-        getComputerChoice(computerSelection);
-        result(computerSelection, playerSelection);
+
+    // Give the game final result
+    finalResult();
+    function finalResult() {
+        if (playerScore >= 3) {
+            console.log('You\'ve won the Rock, Paper, Scissors game !!');
+            return;
+        
+        } else if (computerScore >= 3) {
+            console.log('Oh no, this is so sad, you\'ve lost the Rock, paper, Scissors game');
+            return;
+    
+        } else if (playerScore === computerScore) {
+            console.log('Well, it looks like the computer and you have the same level... Maybe start another game to find out who is the true Rock, Paper, Scissors champion !');
+        }
+        console.log('Here is the final score : '+ 'You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.');
     }
 }
  
-    if (playerScore >= 3) {
-        alert('You\'ve won the Rock, Paper, Scissors game !!');
-    
-    } else if (playerScore < 3) {
-        alert('Oh no, this is so sad, you\'ve lost the Rock, paper, Scissors game');
-
-    } else if (playerScore === computerScore) {
-        alert('Well, it looks like the computer and you have the same level... Maybe start another game to find out who is the true Rock, Paper, Scissors champion !');
-    }
-    console.log('Here is the final score : '+ 'You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.');
+   
 
 // Write a function that plays a single round of Rock paper Scissors
 
 
 
 // Compare the computerChoice and the playerSelection and alert the winner/looser 
-    function result(computerSelection, playerSelection) {
-        if (computerSelection === rock && playerSelection === paper) {
-            playerSelection = win;
-            alert(win + ' Paper beats Rock ');
-        } else if (computerSelection === rock && playerSelection === scissors) {
-            playerSelection = loose;
-            alert(loose + ' Rock beats Scissors');
-        } else if (computerSelection === paper && playerSelection === rock) {
-            playerSelection = loose;
-            alert(loose + ' Paper beats Rock');
-        } else if (computerSelection === paper && playerSelection === scissors) {
-            playerSelection = win;
-            alert(win + ' Scissors beats Paper');
-        } else if (computerSelection === scissors && playerSelection === paper) {
-            playerSelection = loose;
-            alert(loose + ' ' + 'Scissors beats Paper');
-        } else if (computerSelection === scissors && playerSelection === rock) {
-            playerSelection = win;
-            alert(win + ' Rock beats Scissors');
-        } else if (computerSelection === playerSelection) {
-            alert('It\'s a tie.');
-        } 
-
-        roundScore();
-        function roundScore() {
-        if (playerSelection === win) {
-            playerScore = playerScore + 1;
-            console.log('You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.' + ' You\'re ahead !');
-        
-        } else if (playerSelection === loose) {
-            computerScore = computerScore + 1;
-            console.log('You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.' + ' You\'re falling behind...!');
     
-        } else if (playerScore === computerScore) { 
-            console.log('You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.' + ' It looks like a tie so far.')
-        }
 
-    }
-     }
+        
+    //     roundScore();
+    //     function roundScore() {
+    //     if (playerSelection === win) {
+    //         playerScore = playerScore + 1;
+    //         console.log('You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.' + ' You\'re ahead !');
+        
+    //     } else if (playerSelection === loose) {
+    //         computerScore = computerScore + 1;
+    //         console.log('You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.' + ' You\'re falling behind...!');
+    
+    //     } else if (playerScore === computerScore) { 
+    //         console.log('You have ' + playerScore + ' point ' + 'and the computer has ' + computerScore + ' point.' + ' It looks like a tie so far.')
+    //     }
 
-    }
-}
+    // }
+    
+
+ 
